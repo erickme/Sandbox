6cd44d6ada08db17c26d133b10b58bbba1a0cc2c
Erick Mejia <erick.mejia@gmail.com>
2016-09-08 09:57:27 -0400

    Sandbox app
    For now just a skeleton app to use as a template to start an AngularJS
    app

---
+++ app/app.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('sandbox', [
+        // Angular modules 
+        'ngRoute'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/pages/home/home.html'
+            })
+            .when('/page1', {
+                templateUrl: 'app/pages/page1/page1.html'
+            })
+            .when('/page2', {
+                templateUrl: 'app/pages/page2/page2.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+
+    })
+})();
